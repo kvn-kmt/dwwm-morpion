d2e55dd1d96d37d949ad886549250211045904f1
Anturia Hassani <antu.ria-75@live.fr>
2023-04-04 10:58:29 +0200

    ajout des cas gagnant

--- main.js
+++ main.js
@@ -5,6 +5,17 @@ const playerOne = 'X';
 const playerTwo = 'O';
 let playerTurn = playerOne;
 
+const winningPatterns = [
+    [0,1,2], 
+    [3,4,5], 
+    [6,7,8], 
+    [0,3,6], 
+    [1,4,7], 
+    [2,5,8], 
+    [0,4,8], 
+    [2,4,6]
+];
+
 cells.forEach(cell => {
         cell.addEventListener('click', playGame, { once: true });
 });
@@ -24,8 +35,19 @@ function playGame(e) {
     playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
 }
 
-function checkWin(playerTurn) {}
-function checkDraw() {}
+function checkWin(playerTurn) {
+    return winningPatterns.some(comnbination => {
+        return comnbination.every(index =>  {
+            return cells[index].innerHTML == playerTurn;
+        }); 
+    }); 
+}
+
+function checkDraw() {
+    return [...cells].every(cell => {
+        return cell.innerHTML == playerOne || cell.innerHTML == playerTwo;
+    });
+}
 
 function updateGameStatus(status){
     let statusText;
