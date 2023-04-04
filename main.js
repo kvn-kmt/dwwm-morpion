67c4614a40637b36f93f03503b26a9d92c3f434c
Anturia Hassani <antu.ria-75@live.fr>
2023-04-04 09:51:43 +0200

    ajout fonction updateGameStatus, modif fonction playGame

--- main.js
+++ main.js
@@ -12,5 +12,39 @@ cells.forEach(cell => {
 function playGame(e) {
     e.target.innerHTML = playerTurn;
 
+    updateGameStatus(playerTurn); 
     playerTurn == playerOne ? playerTurn = playerTwo : playerTurn = playerOne;
+}
+
+function updateGameStatus(status){
+    let statusText;
+
+    switch (status) {
+        case 'X': 
+            statusText = "Joueur 2 (O), à ton tour !";
+            break;
+        case 'O':
+            statusText = "Joueur 1 (X), c'est à toi !";
+            break;
+        case 'winsX':
+            statusText = "Victoire du Joueur 1 (X) !";
+            break;   
+        case 'winsO':
+            statusText = "Victoire du Joueur 2 (O) !";
+            break;      
+        case 'draw':
+            statusText = "Egalité !";
+            break;            
+    }
+
+    gameStatus.innerHTML = statusText;
+    endGameStatus.innerHTML = statusText;
+}
+
+function endGame() { 
+    document.getElementById('gameEnd') 
+}
+
+function reloadGame() { 
+    window.location.reload() 
 }
